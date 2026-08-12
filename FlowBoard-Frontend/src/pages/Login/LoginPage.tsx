f4417975d78e9d51fd78637cdc382cf608030cb8
SimSeungAh <ng7463@naver.com>
2026-08-12 16:35:40 +0900

    로그인 및 회원가입 라우터 import 수정

--- FlowBoard-Frontend/src/pages/Login/LoginPage.tsx
+++ FlowBoard-Frontend/src/pages/Login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router";
 import { toast } from "sonner";
 
 import { login } from "@/api/auth";
